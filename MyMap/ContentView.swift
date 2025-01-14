9c82122d5773a34676448eab1768b19b1bfecfeb
raiki0710 <0710raiki@gmail.com>
2025-01-14 17:03:38 +0900

    Initial commit

--- MyMap/ContentView.swift
+++ MyMap/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputText: String = ""
+    @State var displaySearchkey: String = "東京駅"
+    @State var displayMapType: MapType = .standard
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("キーワード", text: $inputText, prompt: Text("キーワードを入力してください"))
+                .onSubmit {
+                    displaySearchkey = inputText
+                }
+                .padding()
+            
+            ZStack(alignment: .bottomTrailing) {
+                MapView(searchKey: displaySearchkey, mapType: displayMapType)
+                
+                Button {
+                    
+                    if displayMapType == .standard {
+                        displayMapType = .satellite
+                    } else if displayMapType == .satellite {
+                        displayMapType = .hybrid
+                    } else {
+                        displayMapType = .standard
+                    }
+                } label: {
+                    Image(systemName: "map")
+                        .resizable()
+                        .frame(width: 35.0, height: 35.0)
+                }
+                
+                .padding(.trailing, 20.0)
+                .padding(.bottom, 30.0)
+            }
+            
         }
-        .padding()
     }
 }
 
