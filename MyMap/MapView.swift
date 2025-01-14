df359b9b8d6c0b35920813722a0ca6869f9559c6
raiki0710 <0710raiki@gmail.com>
2025-01-14 17:01:47 +0900

    first commit

---
+++ MyMap/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  MyMap
+//
+//  Created by Raiki Oyatsu on 14/1/2025.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
